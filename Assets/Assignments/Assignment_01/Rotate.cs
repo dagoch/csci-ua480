f84fec667cef490957a6f8f0e5cf970d5b947c7b
SkytronUniverse <boatright.karl@gmail.com>
2018-05-25 15:15:48 -0400

    Files for Testing

---
+++ Assets/Assignments/Assignment_01/Rotate.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour {
+
+    //properties
+    //Transform transform
+    //GameObject gameObject
+    //Vector3
+    public float speed = 10.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //calculate rotation
+        //Debug.Log(transform.eulerAngles);
+        transform.Rotate(Vector3.up * Time.deltaTime * speed);
+        
+	}
+}
