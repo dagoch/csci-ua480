c633cfab07c0944afa178ba6ab605487a30e6d8c
SkytronUniverse <boatright.karl@gmail.com>
2018-05-31 14:59:53 -0400

    Assignment1 Files

--- Assets/Assignments/Assignment_01/A01_kmb826/Scripts/FloorColor.cs
+++ Assets/Assignments/Assignment_01/A01_kmb826/Scripts/FloorColor.cs
@@ -2,17 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FloorColor : MonoBehaviour {
+namespace kmb826
+{
+    public class FloorColor : MonoBehaviour
+    {
 
-    public GameObject gObj;
+        public GameObject gObj;
 
-	// Use this for initialization
-	void Start () {
-        
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        gObj.GetComponent<Renderer>().material.color = Color.green;
-    } 
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            gObj.GetComponent<Renderer>().material.color = Color.green;
+        }
+    }
 }
