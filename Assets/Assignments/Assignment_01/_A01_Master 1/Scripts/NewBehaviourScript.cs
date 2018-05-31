dba47f644da231dffad61f4255973d7c32af6869
Zhongheng Sun <zhonghengsun@Zhonghengs-Air.fios-router.home>
2018-05-31 16:27:02 -0400

    done

---
+++ Assets/Assignments/Assignment_01/_A01_Master 1/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace A01zs967
+{
+    public class NewBehaviourScript : MonoBehaviour
+    {
+
+        public Rigidbody rb;
+
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            rb.AddForce(0, 0, 2000 * Time.deltaTime);
+        }
+    }
+}
