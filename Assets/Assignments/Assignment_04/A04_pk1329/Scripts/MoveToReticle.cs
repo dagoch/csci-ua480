3baa131cbc10886fb8c6e0434e2c6f3fa0eb7ea1
peterckim <peterckim96@gmail.com>
2018-06-28 19:29:34 -0400

    begin assignment 4

---
+++ Assets/Assignments/Assignment_04/A04_pk1329/Scripts/MoveToReticle.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace pk1329A04
+{
+    public class MoveToReticle : MonoBehaviour
+    {
+
+        public float speed;
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (Input.GetMouseButton(0))
+            {
+                transform.Translate(Camera.main.transform.forward * speed * Time.deltaTime);
+            }
+        }
+    }
+}
