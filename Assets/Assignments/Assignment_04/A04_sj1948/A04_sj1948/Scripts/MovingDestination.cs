6d36eabb788f1a31d219e77620640ebdfa7deaa8
FrankJiang1208 <39501874+FrankJiang1208@users.noreply.github.com>
2018-07-05 14:43:30 -0400

    Add files via upload

---
+++ Assets/Assignments/Assignment_04/A04_sj1948/A04_sj1948/Scripts/MovingDestination.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+
+//similar Code as used in class with a few adjustments
+namespace A04sj1948
+{
+    [RequireComponent(typeof(Collider))]
+    public class MovingDestination : MonoBehaviour, IPointerClickHandler
+    {
+        [Tooltip("How long does the player need to get here")]
+        public float RequiredMovingTime;
+
+        private Collider _collider;
+
+		private void Start()
+		{
+            _collider = GetComponent<Collider>();
+		}
+
+		void IPointerClickHandler.OnPointerClick(PointerEventData eventData)
+        {
+            //get the magnitude of the camera to the location you are clicking on the ground
+            float distance=Vector3.Magnitude(eventData.pointerPressRaycast.worldPosition - Camera.main.transform.position);
+
+            //if it is within 10 meters then allow teleportation 
+            if(distance<=10){
+                PlayerController.Instance.MoveToPosition(eventData.pointerPressRaycast.worldPosition, RequiredMovingTime);
+            }
+           
+        }
+
+		private void OnTriggerEnter(Collider other)
+		{
+            //Disable the collision detection on this collider when the camera is inside of it, so the casting ray won't be blocked by it.
+            if (other.CompareTag("MainCamera")) {
+                _collider.isTrigger = true;
+            }
+		}
+
+		private void OnTriggerExit(Collider other)
+		{
+            //Re-enable the collision detection when camera exits the collider
+            if (other.CompareTag("MainCamera"))
+            {
+                _collider.isTrigger = false;
+            }
+		}
+	}
+}
---
+++ Assets/Assignments/Assignment_04/A04_sj1948/A04_sj1948/Scripts/SpawnObject.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnObject : MonoBehaviour {
+    public Vector3 center;
+    public Vector3 size;
+    public GameObject cubeSpawn;
+    public GameObject sphereSpawn;
+    private Material matOfObject;
+    public int amount;
+
+	// Use this for initialization
+	void Start () {
+        spawn(amount);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = new Color(1,0,0,5f);
+        Gizmos.DrawCube(center,size);
+
+    }
+    private void spawn(int number){
+        for (int i = 0; i < number;i++){
+            float cubeSize = Random.Range(0, size.x / 40);
+            float sphereSize = Random.Range(0, size.x / 20);
+            Vector3 pos1 = center + new Vector3(Random.Range(-size.x / 2, size.x / 2), Random.Range(-size.y / 2, size.y / 2), Random.Range(-size.z / 2, size.z / 2));
+            Vector3 pos2 = center + new Vector3(Random.Range(-size.x / 2, size.x / 2), Random.Range(-size.y / 2, size.y / 2), Random.Range(-size.z / 2, size.z / 2));
+            GameObject rep1=Instantiate(cubeSpawn, pos1, Quaternion.Euler(Random.Range(0.0f, 360.0f), Random.Range(0.0f, 360.0f), Random.Range(0.0f, 360.0f)));
+            GameObject rep2 = Instantiate(sphereSpawn, pos2, Quaternion.Euler(Random.Range(0.0f, 360.0f), Random.Range(0.0f, 360.0f), Random.Range(0.0f, 360.0f)));
+            rep1.transform.localScale=new Vector3(cubeSize, cubeSize, cubeSize);
+            rep1.GetComponent<MeshRenderer>().material.SetColor("_Color", new Color(Random.Range(0, 8),Random.Range(0, 8),Random.Range(0, 8),Random.Range(0, 8)));
+            rep2.transform.localScale = new Vector3(cubeSize, cubeSize, cubeSize);
+            rep2.GetComponent<MeshRenderer>().material.SetColor("_Color", new Color(Random.Range(0, 8), Random.Range(0, 8), Random.Range(0, 8), Random.Range(0, 8)));
+
+        }
+       
+    }
+
+}
