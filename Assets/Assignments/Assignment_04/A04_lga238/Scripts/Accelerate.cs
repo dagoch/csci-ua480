8e9001efaff60f83d9429c1c6e9ac1b96b91d41b
Louis Ashurst <Ash@Louiss-MacBook-Air-3.local>
2018-07-09 20:29:38 -0400

    assignment04

---
+++ Assets/Assignments/Assignment_04/A04_lga238/Scripts/Accelerate.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace lga238 {
+public class Accelerate : MonoBehaviour {
+	public float speed;
+	public float currentSpeed;
+	private bool moving = false;
+	private bool easingMovement = false;
+
+	void Start () {
+		speed = 8.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if(Input.GetMouseButton(0)){
+			moving = true;
+
+			if (easingMovement){
+				//accelarate
+				currentSpeed = currentSpeed + (0.1f * speed);
+			}
+			else{
+				transform.position+= Camera.main.transform.forward * speed * Time.deltaTime;
+			}
+		}
+
+
+		else if(moving){
+			//decellerate
+		}
+	}
+}
+}
---
+++ Assets/Assignments/Assignment_04/A04_lga238/Scripts/PlaceCyl.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace lga238 {
+public class PlaceCyl : MonoBehaviour {
+
+	// Use this for initialization
+
+	public GameObject cyl;
+	void Start () {
+		for(int i = 0; i < 10; i++){
+			GameObject temp = GameObject.Instantiate(cyl, new Vector3(Random.Range(-240f,240f),7f, Random.Range(-240f,240f)), Quaternion.identity);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
+}
