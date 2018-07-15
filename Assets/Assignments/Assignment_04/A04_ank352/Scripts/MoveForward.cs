901773b4779e254ac1adcf39ac77fd85a1222996
Alexander Kessaris <ank352@nyu.edu>
2018-07-15 18:55:16 -0400

    A04 - Alexander Kessaris (#105)
    * Added files for Assignment 01
    
    * Add files via upload
    
    * Assignment 02
    
    * Add files via upload
    
    * Assignment 3
    
    Main changes for assignment can be found in the following scripts:
    MenuCavasController.cs
    PickupMe.cs
    ScaleSliderController.cs
    
    * Assignment 4
    
    * Added terrain file
    
    I had a reference to a terrain for my scene, but I had it outside of my A04_ank352 folder. It should be fine now for testing.

---
+++ Assets/Assignments/Assignment_04/A04_ank352/Scripts/MoveForward.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace A04_ank352
+{
+  public class MoveForward : MonoBehaviour {
+
+      public float speed = 10;
+      public bool easingMovement = true;
+      private bool isMoving = false;
+      private float currentSpeed = 0.0f;
+      private float bounds = 60f;
+
+  	// Use this for initialization
+  	void Start () {
+
+  	}
+
+  	// Update is called once per frame
+  	void Update () {
+        inBounds(transform.position);
+
+        if (Input.GetMouseButton(0)) {
+            isMoving = true;
+
+            if (easingMovement)
+            {
+                // accelerate to speed
+                currentSpeed = Mathf.Lerp(currentSpeed, speed, Time.deltaTime);   //currentSpeed + (.01f * speed);
+                Vector3 forward = Camera.main.transform.forward;
+                forward.y = 0;
+                transform.Translate(forward*currentSpeed * Time.deltaTime);
+
+            }
+            else
+            {
+                Vector3 forward = Camera.main.transform.forward;
+                forward.y = 0;
+                transform.Translate(forward * speed * Time.deltaTime);
+            }
+        } else if (isMoving && easingMovement) {
+            // decelerate
+            currentSpeed = currentSpeed - (.01f * speed);
+            Vector3 forward = Camera.main.transform.forward;
+            forward.y = 0;
+            transform.Translate(forward*currentSpeed * Time.deltaTime);
+            if (currentSpeed <= 0)
+              isMoving = false;
+        }
+  	   }
+       Vector3 inBounds(Vector3 currentPosition) {
+         // Debug.Log(currentPosition);
+         if (currentPosition.x > bounds) {
+           currentPosition.x = bounds;
+           Debug.Log(true);
+         }
+
+         if (currentPosition.z > bounds)
+          currentPosition.z = bounds;
+
+         if (currentPosition.x < -bounds)
+          currentPosition.x = -bounds;
+
+         if (currentPosition.z < -bounds)
+          currentPosition.z = -bounds;
+
+          return currentPosition;
+       }
+  }
+}
---
+++ Assets/Assignments/Assignment_04/A04_ank352/Scripts/PlayerController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace A04_ank352
+{
+    public class PlayerController : MonoBehaviour
+    {
+        public static PlayerController Instance;
+
+		private void Awake()
+		{
+            //Singleton
+            if (Instance == null) {
+                Instance = this;
+            } else if (Instance != this) {
+                Destroy(this);
+            }
+		}
+
+        public void Translate(Vector3 translation) {
+            transform.Translate(translation, Space.World);
+        }
+
+        public void MoveToPosition(Vector3 des, float time)
+        {
+            StopAllCoroutines();
+            des.y = transform.position.y;
+
+            //If the distance of the travel point is greater than 10, move gradually
+            if (Mathf.Abs(des.z - transform.position.z) > 10 || Mathf.Abs(des.x - transform.position.x) > 10)
+              time = 5;
+            //Otherwise, teleport
+            else
+              time = 0;
+            StartCoroutine(MoveToPositionGradually(des, time));
+        }
+
+        private IEnumerator MoveToPositionGradually(Vector3 des, float time) {
+          float t = 0.0f;
+          Vector3 initialPos = transform.position;
+          while (t < time) {
+              t += Time.smoothDeltaTime;
+              transform.position = initialPos + (des - initialPos) * Mathf.Lerp(0.0f, 1.0f, t / time);
+              yield return null;
+          }
+          transform.position = des;
+        }
+    }
+}
