61f577bf911bf22ca1cca7d13adf52f88c230a3f
Anthony Ramirez <ar4477@nyu.edu>
2018-07-19 16:59:03 -0400

    added assignment 7

---
+++ Assets/Assignments/Assignment_07/Assets/Scripts/Billboard.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace ar4477.A07
+{
+  // Script modeled after Unity tutorial
+  public class Billboard : MonoBehaviour
+  {
+
+      void Update()
+      {
+          transform.LookAt(Camera.main.transform);
+      }
+  }
+}
---
+++ Assets/Assignments/Assignment_07/Assets/Scripts/Bullet.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace ar4477.A07
+{
+  // Script modeled after Unity tutorial
+  public class Bullet : MonoBehaviour
+  {
+
+      void OnCollisionEnter(Collision collision)
+      {
+          var hit = collision.gameObject;
+          var health = hit.GetComponent<Health>();
+          if (health != null)
+          {
+              health.TakeDamage(10);
+          }
+
+          Destroy(gameObject);
+      }
+  }
+}
