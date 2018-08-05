8e645bb40fcea32ea365d6ca500a3228451e2d30
dagoch <dagoch@gmail.com>
2018-08-05 02:07:55 -0400

    Added NetExample-SyncedData scene, showing how to sync data across all clients

---
+++ Assets/Assignments/Assignment_07/_A07_Master/Scripts/GameManager.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+namespace A07Examples
+{
+    public class GameManager : NetworkBehaviour
+    {
+
+        [SyncVar]
+        public int lastPlayerId = 0;
+
+        [SyncVar]
+        public int buttonCount = 0;
+
+        public Text LastIdText;
+        public Text ScorebdText;
+
+        public SyncListInt players = new SyncListInt();
+
+
+        // Use this for initialization
+        void Start()
+        {
+        }
+
+
+        public override void OnStartServer()
+        {
+            base.OnStartServer();
+
+            players.Add(999);  // add dummy 0th player to score list: this is just for debugging
+
+        }
+
+
+        // Update is called once per frame
+        // This runs on all clients
+        void Update()
+        {
+            LastIdText.text = "Last Id: "+lastPlayerId.ToString();
+
+            // Display scores for each player
+            string board = "Scoreboard\n";
+            for (int i = 0; i < players.Count; i++)
+                board += i.ToString() + ": " + players[i].ToString() +"\n";
+            
+            ScorebdText.text = board;
+        }
+
+        public void PressButton(int playerId) {
+            buttonCount++;
+            Debug.Log("Button pressed, count = " + buttonCount);
+            players[playerId]++;
+
+        }
+
+        // this increments lastPlayerId and adds an entry in the scoreboard array
+        public void AddNewPlayer() {
+            lastPlayerId++;
+            players.Add(0);
+            Debug.Log("Added new player: " + lastPlayerId + " now players len = " + players.Count);
+        }
+    }
+}
---
+++ Assets/Assignments/Assignment_07/_A07_Master/Scripts/KeybdPlayerController.cs
@@ -0,0 +1,137 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+namespace A07Examples
+{
+    public class KeybdPlayerController : NetworkBehaviour
+    {
+
+        public float rotSpeed = 2.0f;
+        public float transSpeed = 0.5f;
+
+        [SyncVar(hook = "OnPlayerIdChange")]
+        public int playerId;
+
+        // if you have a SyncVar hook, the variable itself is not changed directly;
+        // instead you get the new value in the hook function and need to set the var yourself
+        void OnPlayerIdChange(int id)
+        {
+            playerId = id;
+            Debug.Log("Player id set on player " + playerId );
+
+            idText.text = playerId.ToString();
+        }
+
+
+        public Text idText;
+
+
+        public GameManager gameManager;
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        public override void OnStartServer()
+        {
+            base.OnStartServer();
+            Debug.Log("Get GameManager on Server");
+
+            while (gameManager == null)
+            {
+                GameObject temp = GameObject.Find("Game Manager");
+                if (temp != null)
+                    gameManager = temp.GetComponent<GameManager>();
+            }
+
+        }
+
+        public override void OnStartClient()
+        {
+            base.OnStartClient();
+
+            // this is a non-local player object.  it has received an id from the server;
+            // need to set my text to show it.
+            idText.text = playerId.ToString();
+
+            Debug.Log("Client non-local player started with id: " + playerId);
+
+        }
+
+
+        public override void OnStartLocalPlayer()
+        {
+            GetComponent<Renderer>().material.color = Color.blue;
+            Debug.Log("Get GameManager on Player");
+            while (gameManager == null)
+            {
+                GameObject temp = GameObject.Find("Game Manager");
+                if (temp != null)
+                    gameManager = temp.GetComponent<GameManager>();
+            }
+            // Tell server a new player has started
+                CmdIncrPlayerId();
+        }
+
+        // This Increments the player count on the server
+        [Command]
+        void CmdIncrPlayerId() {
+            Debug.Log("Incrementing lastPlayerId");
+            gameManager.AddNewPlayer();   // this increments lastPlayerId and adds an entry in the scoreboard array
+
+            // Here I am letting the server send the new value via the syncvar
+            playerId = gameManager.lastPlayerId;
+
+            // Alternately I can call an Rpc method on the client and send it the player id
+//            RpcSetId(gameManager.lastPlayerId);
+        }
+
+        // this is alternate to using syncvar: can use RPC to set value on each client
+        [ClientRpc]  // called on the Server, but invoked on the Clients
+        void RpcSetId(int id)
+        {
+                Debug.Log("Player got rpcsetid = " + id);
+                playerId = id;
+                idText.text = playerId.ToString();
+
+        }
+
+
+        // Update is called once per frame
+        void Update()
+        {
+
+//            idText.text = playerId.ToString();
+
+            if (!isLocalPlayer) {
+                return;
+            }
+
+
+
+            float moveX = Input.GetAxis("Horizontal");
+            float moveZ = Input.GetAxis("Vertical");
+
+            transform.Rotate(transform.up, moveX * rotSpeed);
+            transform.Translate(0, 0, moveZ * transSpeed);
+
+            if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonUp(0)) // || Input.GetMouseButton(0)
+            {
+                CmdCountMe();
+            }
+
+        }
+
+        [Command]
+        void CmdCountMe() {
+            gameManager.PressButton(playerId);
+        }
+
+
+    }
+}
